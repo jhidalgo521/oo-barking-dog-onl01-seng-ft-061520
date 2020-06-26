6b4f5870119d0a9791777324aad06556cdca80db
Jose Hidago <jhidalgo521@gmail.com>
2020-06-26 11:51:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,14 +2,23 @@
 class Dog 
   
   def name= (dog_name)
-    
+  
     @this_dog = dog_name
   
   end 
   
   def name
+  
     @this_dog 
+  
   end 
+  
+  def bark
+    
+    puts "woof!"
+  
+  end
+  
   
   
 end 
